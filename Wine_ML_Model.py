7d969a8612d1aa36f8289bc72f81d45bc8abad37
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2020-08-27 11:24:28 -0300

    Aug 27th Edits

---
+++ Wine_ML_Model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#read in the data
+white = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", sep=';')
+red = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep=';')
+
+# Pre process the data and begin creating the information for the neural network. This is where we are labeling the data (ie. hot encoding ) and joining the data
+
+# add a type column for red with value 1
+red['type'] = 1
+
+# add a type column  for white with a value of 0
+white['type'] = 0
+
+# Append 'white' to 'red' 
+wine = red.append(white, ignore_index=True)
+
+print(wine.head())
+
+# Use the created data to come up with a machine learning model specfically using sklearn
