8885aedcc91ddb51388fcd8b76c611d8dffc595d
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2020-08-27 12:29:39 -0300

    Created Venv, installed sklearn

--- Wine_ML_Model.py
+++ Wine_ML_Model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as npp
+from sklearn.model_selection import train_test_split
 
 
 #read in the data
@@ -20,3 +22,12 @@ wine = red.append(white, ignore_index=True)
 print(wine.head())
 
 # Use the created data to come up with a machine learning model specfically using sklearn
+
+# Specify the data
+X = wine.ix[:,0:11]
+
+# Specify the target labels and flatten the array
+y = np.ravel(wine.type)
+
+# Split the data in train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
